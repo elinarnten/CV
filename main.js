fd43396dcc6ef58be6070b98b353fa2ede49e9a4
elinarnten <elin.arnten@hotmail.com>
2021-12-02 17:42:36 +0100

    moved the html links to javascript file

--- main.js
+++ main.js
@@ -5,37 +5,40 @@ function main() {
 }
 function addEventListeners() {
     document.onclick = hideMenu;
-    const myName = document.getElementById('name');
 
-    myName.onclick = getAboutMe;
-    
+    const myName = document.getElementById('name');
+    myName.onclick = getStartPage;
+
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.onclick = showMenu;
 
-    const mobileResume = document.getElementById('mobile-resume');
-    mobileResume.onclick = getResume;
+    const resume = document.getElementById('resume');
+    resume.onclick = getResume;
 
-    const mobilePortfolio = document.getElementById('mobile-portfolio');
-    mobilePortfolio.onclick = getPortfolio;
+    const portfolio = document.getElementById('portfolio');
+    portfolio.onclick = getPortfolio;
     
-    const mobileAboutMe = document.getElementById('mobile-aboutMe');
-    mobileAboutMe.onclick = getAboutMe;
+    const aboutMe = document.getElementById('aboutMe');
+    aboutMe.onclick = getAboutMe;
 }
 
 function showMenu(event) {
-    document.getElementById('mobile-menu-nav').classList.toggle('show')
+    document.getElementById('mobile-menu-nav').classList.toggle('show');
     event.stopPropagation();
 
 }
 function hideMenu() {
-    document.getElementById('mobile-menu-nav').classList.remove('show')
+    document.getElementById('mobile-menu-nav').classList.remove('show');
 }
 function getResume() {
-
+    window.location.href = "http://127.0.0.1:5501/resume.html";
 }
 function getPortfolio() {
-
+    window.location.href = "http://127.0.0.1:5501/portfolio.html";
 }
 function getAboutMe() {
+    window.location.href = "http://127.0.0.1:5501/aboutMe.html";
+}
+function getStartPage() {
     window.location.href = "http://127.0.0.1:5501/index.html";
 }
