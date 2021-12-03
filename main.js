f71a7a1a8ce1a1023da0b51e2bdecefd42ad4f56
elinarnten <elin.arnten@hotmail.com>
2021-12-03 11:07:47 +0100

    new test

--- main.js
+++ main.js
@@ -31,8 +31,7 @@ function hideMenu() {
     document.getElementById('mobile-menu-nav').classList.remove('show');
 }
 function getResume() {
-    window.location.href = "http://127.0.0.1:5501/resume.html", "/resume.html";
-    //window.location.href = "elinarnten.github.io/Laboration2/resume.html"
+    window.location.href = "http://127.0.0.1:5501/resume.html";
 }
 function getPortfolio() {
     window.location.href = "http://127.0.0.1:5501/portfolio.html";
