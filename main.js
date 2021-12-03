0c15c2958ddcb1b067e64fc7ccf92a709c23c494
elinarnten <elin.arnten@hotmail.com>
2021-12-03 11:16:20 +0100

    new test

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ function hideMenu() {
     document.getElementById('mobile-menu-nav').classList.remove('show');
 }
 function getResume() {
-    window.location.href = "http://127.0.0.1:5501/resume.html";
+    //window.location.href = "http://127.0.0.1:5501/resume.html";
+    window.location.href = "https://elinarnten.github.io/Laboration2/resume.html"
 }
 function getPortfolio() {
     window.location.href = "http://127.0.0.1:5501/portfolio.html";
