e6c5eb1d3a2cd43599a85488e95ec65007c1f1e3
elinarnten <elin.arnten@hotmail.com>
2021-12-03 17:47:04 +0100

    js-doc comment

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+/**
+ * This is where the program begins
+ */
 window.onload = main;
-
 function main() {
    addEventListeners();
 }
+
+/**
+ * Listens to the users click command and calls the right function
+ */
 function addEventListeners() {
     document.onclick = hideMenu;
 
@@ -22,26 +28,49 @@ function addEventListeners() {
     aboutMe.onclick = getAboutMe;
 }
 
+/**
+ * This function will show the mobile menu
+ * @param {object} event 
+ */
 function showMenu(event) {
     document.getElementById('mobile-menu-nav').classList.toggle('show');
     event.stopPropagation();
 
 }
+/**
+ * This function will hide the menu
+ */
 function hideMenu() {
     document.getElementById('mobile-menu-nav').classList.remove('show');
 }
+
+/**
+ * This function will get the user to resume page
+ */
 function getResume() {
     //window.location.href = "http://127.0.0.1:5501/resume.html";
     window.location.href = "https://elinarnten.github.io/Laboration2/resume.html"
 }
+
+/**
+ * This function will get the user to portfolio page
+ */
 function getPortfolio() {
     //window.location.href = "http://127.0.0.1:5501/portfolio.html";
     window.location.href = "https://elinarnten.github.io/Laboration2/portfolio.html"
 }
+
+/**
+ * This function will get the user to About me page
+ */
 function getAboutMe() {
     //window.location.href = "http://127.0.0.1:5501/aboutMe.html";
     window.location.href = "https://elinarnten.github.io/Laboration2/aboutMe.html"
 }
+
+/**
+ * This function will get the user to start page
+ */
 function getStartPage() {
     //window.location.href = "http://127.0.0.1:5501/index.html";
     window.location.href = "https://elinarnten.github.io/Laboration2/"
